01b8305c308f41709a48d097545881ed8955af0d
Cameron Breze <cbreze@arielmedicine.com>
2019-02-20 18:30:02 -0500

    Added variant input fxn to the GUI

--- duct_model_events.py
+++ duct_model_events.py
@@ -15,6 +15,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5 import QtCore, QtGui, QtWidgets
 import pandas as pd
+import numpy as np
 
 # Duct Model MainWindow
 from duct_model_gui import Ui_MainWindow
@@ -39,8 +40,13 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
 		# Variant Options
 		self.variant_ops = pd.read_csv('cutting_variants.csv')['variant']
+		self.variant_dict = dict()
+		for i in range(len(self.variant_ops)):
+			self.variant_dict[pd.read_csv('cutting_variants.csv')['variant'][i]] = pd.read_csv('cutting_variants.csv')['wt_func'][i]
 		self.comboBox_variant_input_area.addItem('-')
 		self.comboBox_variant_input_area.addItems(self.variant_ops)
+
+		self.wt_total = 100
 
 		# List of Patient Variants
 		self.variant_set = set()
@@ -52,7 +58,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 	def string_of_variants(self, variant_list):
 		output_string = ''
 		for item in variant_list:
-			output_string += (item +'\n')
+			output_string += (item + ' %f' % round(self.variant_dict[item],2) + '%' + '\n')
 		return output_string
 
 	def connect_elements(self):
@@ -62,6 +68,22 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 		self.pushButton_add_variant.clicked.connect(self.add_variant)
 		self.pushButton_remove_variant.clicked.connect(self.remove_variant)
 
+	def update_wt_func(self):
+		self.progressBar_wt_functionality.setValue(self.wt_total)
+
+	def collect_variant_impact(self):
+		impact_percentage = 0
+		variant_impacts = []
+
+		for item in self.patient_variants:
+			variant_impacts.append(float(self.variant_dict[item]))
+		impact_percentage = np.mean(variant_impacts)
+		print(impact_percentage)
+		if impact_percentage > 100:
+			impact_percentage = 100
+			print('Variants perform above expected WT functionality by %f percent' % (impact_percentage-100))
+		return impact_percentage
+
 	def add_variant(self):
 		variant = self.comboBox_variant_input_area.currentText()
 		if variant != '-':
@@ -70,6 +92,8 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 				text = self.string_of_variants(self.patient_variants)
 				self.plainTextEdit_patient_variants.setPlainText(text)
 				self.variant_set.add(variant)
+				self.wt_total = self.collect_variant_impact()
+				self.update_wt_func()
 
 	def remove_variant(self):
 		variant = self.comboBox_variant_input_area.currentText()
@@ -79,6 +103,8 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 				text = self.string_of_variants(self.patient_variants)
 				self.plainTextEdit_patient_variants.setPlainText(text)
 				self.variant_set.remove(variant)
+				self.wt_total = self.collect_variant_impact()
+				self.update_wt_func()
 
 	
 	def update_patient_variants(self):
